a5baa685e48a4a703b00396a354897b05fa154db
Marcio Garcia <marcio.mangar@gmail.com>
2016-08-03 19:47:55 -0300

    02

--- CodeSchoolAngular2/02/app/main.js
+++ CodeSchoolAngular2/02/app/main.js
@@ -13,17 +13,55 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Ultra Racing';
-        this.carPart = {
-            "id": 1,
-            "name": "Super Tires",
-            "description": "These tires are the very best",
-            "inStock": 5
-        };
+        this.carParts = [{
+                "id": 1,
+                "name": "Super Tires",
+                "description": "These tires are the very best",
+                "price": 50,
+                "inStock": 5
+            },
+            {
+                "id": 2,
+                "name": "Aerofolio",
+                "description": "These tires are the very best",
+                "price": 5,
+                "inStock": 0
+            },
+            {
+                "id": 3,
+                "name": "Farol de Milha",
+                "description": "These tires are the very best",
+                "price": 12,
+                "inStock": 2
+            },
+            {
+                "id": 4,
+                "name": "Maçaneta",
+                "description": "These tires are the very best",
+                "price": 14,
+                "inStock": 0
+            },
+            {
+                "id": 5,
+                "name": "Filtro de Óleo da Primária",
+                "description": "These tires are the very best",
+                "price": 40,
+                "inStock": 4
+            },
+        ];
     }
+    AppComponent.prototype.totalCarParts = function () {
+        var sum = 0;
+        for (var _i = 0, _a = this.carParts; _i < _a.length; _i++) {
+            var carPart = _a[_i];
+            sum += carPart.inStock;
+        }
+        return sum;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n  <h2>{{carPart.name}}</h2>\n  <p>{{carPart.description}}</p>\n  <p>{{carPart.inStock}}</p>\n  "
+            template: "<h1>{{title}} - 02</h1>\n  <p>There are {{totalCarParts()}} car parts.</p>\n  <ul>\n    <li *ngFor=\"let carPart of carParts\">\n      <h2>{{carPart.name | uppercase}}</h2>\n      <p>{{carPart.description}}</p>\n      <p>{{carPart.price | currency:'BRL':true:'1.2-2'}}</p>\n      <p *ngIf=\"carPart.inStock > 0\">Quantidade em estoque: {{carPart.inStock}}</p>\n      <p *ngIf=\"carPart.inStock === 0\"><b>out of stock</b></p>\n     </li>\n  </ul>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- CodeSchoolAngular2/02/app/main.ts
+++ CodeSchoolAngular2/02/app/main.ts
@@ -3,20 +3,64 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  <h2>{{carPart.name}}</h2>
-  <p>{{carPart.description}}</p>
-  <p>{{carPart.inStock}}</p>
+  template: `<h1>{{title}} - 02</h1>
+  <p>There are {{totalCarParts()}} car parts.</p>
+  <ul>
+    <li *ngFor="let carPart of carParts">
+      <h2>{{carPart.name | uppercase}}</h2>
+      <p>{{carPart.description}}</p>
+      <p>{{carPart.price | currency:'BRL':true:'1.2-2'}}</p>
+      <p *ngIf="carPart.inStock > 0">Quantidade em estoque: {{carPart.inStock}}</p>
+      <p *ngIf="carPart.inStock === 0"><b>out of stock</b></p>
+     </li>
+  </ul>
   `
 })
 class AppComponent {  
   title = 'Ultra Racing';
 
-  carPart = {
+  carParts = [{
     "id": 1,
     "name": "Super Tires",
     "description": "These tires are the very best",
+    "price": 50,
     "inStock": 5
+  },
+  {
+    "id": 2,
+    "name": "Aerofolio",
+    "description": "These tires are the very best",
+    "price": 5,
+    "inStock": 0
+  },
+  {
+    "id": 3,
+    "name": "Farol de Milha",
+    "description": "These tires are the very best",
+    "price": 12,
+    "inStock": 2
+  },
+  {
+    "id": 4,
+    "name": "Maçaneta",
+    "description": "These tires are the very best",
+    "price": 14,
+    "inStock": 0
+  },
+  {
+    "id": 5,
+    "name": "Filtro de Óleo da Primária",
+    "description": "These tires are the very best",
+    "price": 40,
+    "inStock": 4
+  },  
+  ];
+  totalCarParts() {
+    let sum = 0;
+    for (let carPart of this.carParts) {
+      sum += carPart.inStock;
+    }
+    return sum;
   }
 
 }
