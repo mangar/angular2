aa2c9affa8885507eda0cce65eea786031e1f861
Marcio Mangar <marcio.mangar@gmail.com>
2016-08-18 00:44:07 -0300

    basic class for services
    Signed-off-by: Marcio Mangar <marcio.mangar@gmail.com>

---
+++ CodeSchoolAngular2/07/src/app/extra/basic.services.ts
@@ -0,0 +1,8 @@
+export class BasicServices {
+  public basicUrl: string;
+
+  constructor() {
+    this.basicUrl = '';
+  }
+
+}
--- CodeSchoolAngular2/07/src/app/includes/sidebar/services/projects.services.ts
+++ CodeSchoolAngular2/07/src/app/includes/sidebar/services/projects.services.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+
+// import { BasicServices } from ''../../../extra/basic.services'
 import { Project } from './models/project';
 
 
